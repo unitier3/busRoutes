ac8a22b8f2f30506245cd0cbafafe6865da4aa70
WILLIAMS <Q5110442@live.tees.ac.uk>
2019-10-29 15:19:31 +0000

    tmp

--- src/projects/depth.clj
+++ src/projects/depth.clj
@@ -18,15 +18,17 @@
   ([state goal lmg map been]
    (cond
      (= (:state state) goal )
-     (do (conj been (:state state)) state)
+       (do (conj been (:state state)) state)
      (= (count (set been)) (allStations map))
-     false
+       false
      (empty? (removeBeenValues (lmg map state) been))
-     (do (println state " - FAIL")
+       (do
+         (println state " - FAIL")
          (depth-firstSearch {:state (last been) :cost (- (state :cost) (fixPrice map (:state state) (last been)))} goal lmg map (conj been (:state state))))
      :else
-     (do (println state)
-         (depth-firstSearch (first (removeBeenValues (lmg map state) been)) goal lmg map (conj been (:state state)))))))
+     (do
+       (println state)
+       (depth-firstSearch (first (removeBeenValues (lmg map state) been)) goal lmg map (conj been (:state state)))))))
 
 ;;(depth-firstSearch {:state "newcastle" :cost 0} "chester" bestFirstLMG busRoutes00)
 ;;Find out Elapsed time for best-firstSearchA to run:
